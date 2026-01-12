ce341ccce43a7a1ef67d6eb07b677cefcde387fe
anerilohakane <aneridelxn@gmail.com>
2026-01-12 15:06:01 +0530

    Supplier Registration

--- models/Supplier.js
+++ models/Supplier.js
@@ -169,8 +169,19 @@ supplierSchema.methods.isPasswordCorrect = async function (password) {
 supplierSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     { id: this._id, role: this.role },
-    process.env.JWT_ACCESS_SECRET,
-    { expiresIn: "1d" }
+    process.env.JWT_ACCESS_SECRET || "fallback_access_token_secret",
+    { expiresIn: process.env.JWT_ACCESS_EXPIRY || "1d" }
+  );
+};
+
+/* ---------------------------------
+   Generate Refresh Token
+---------------------------------- */
+supplierSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    { id: this._id },
+    process.env.REFRESH_TOKEN_SECRET || "fallback_refresh_token_secret",
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY || "7d" }
   );
 };
 
