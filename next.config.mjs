6d9341d769ea4242c7f427adfee6bffa1157cfea
anerilohakane <aneridelxn@gmail.com>
2026-01-12 13:53:31 +0530

    Remove bycrypt

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,21 @@ const nextConfig = {
   reactCompiler: true,
   serverExternalPackages: ['jsonwebtoken', 'bcryptjs'],
 
+
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Replace with specific origin if needed, e.g. "http://localhost:3000"
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
